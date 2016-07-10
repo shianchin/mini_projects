6a8382c1e7db41398e0ac2a703289ae481e32bd0
shianchin <shianchin3@gmail.com>
2016-07-11 00:18:15 +0800

    Add quotation around dir path.

--- auto_backup/auto_backup.cmd
+++ auto_backup/auto_backup.cmd
@@ -1,5 +1,5 @@
-robocopy C:\Users\user\Documents F:\Backup\Documents /MIR /DCOPY:T /xj /np /tee /log:backup_log.txt
-robocopy C:\Users\user\Pictures  F:\Backup\Pictures  /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
-robocopy C:\Users\user\Music     F:\Backup\Music     /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
-robocopy C:\Users\user\Downloads F:\Backup\Downloads /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+robocopy "C:\Users\user\Documents" "F:\Backup\Documents" /MIR /DCOPY:T /xj /np /tee /log:backup_log.txt
+robocopy "C:\Users\user\Pictures"  "F:\Backup\Pictures"  /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+robocopy "C:\Users\user\Music"     "F:\Backup\Music"     /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+robocopy "C:\Users\user\Downloads" "F:\Backup\Downloads" /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
 pause
