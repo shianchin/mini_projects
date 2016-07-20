e78df224c35d2342a0a0d1f63a26e9365e643d3c
shianchin <shianchin3@gmail.com>
2016-07-20 23:05:57 +0800

    Add dir checking.

--- auto_backup/auto_backup.cmd
+++ auto_backup/auto_backup.cmd
@@ -1,5 +1,15 @@
-robocopy "C:\Users\user\Documents" "F:\Backup\Documents" /MIR /DCOPY:T /xj /np /tee /log:backup_log.txt
-robocopy "C:\Users\user\Pictures"  "F:\Backup\Pictures"  /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
-robocopy "C:\Users\user\Music"     "F:\Backup\Music"     /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
-robocopy "C:\Users\user\Downloads" "F:\Backup\Downloads" /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+@echo off
+
+IF EXIST "F:\Backup\NUL" (
+    IF EXIST "C:\Users\user\" (
+    robocopy "C:\Users\user\Documents" "F:\Backup\Documents" /MIR /DCOPY:T /xj /np /tee /log:backup_log.txt
+    robocopy "C:\Users\user\Pictures"  "F:\Backup\Pictures"  /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+    robocopy "C:\Users\user\Music"     "F:\Backup\Music"     /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+    robocopy "C:\Users\user\Downloads" "F:\Backup\Downloads" /MIR /DCOPY:T /xj /np /tee /log+:backup_log.txt
+    ) ELSE (
+    echo ERROR: Source directory "C:\Users\user\" not found.
+    )
+) ELSE (
+echo ERROR: Destination directory "F:\Backup\" not found.
+)
 pause
